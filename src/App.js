92244b2212e2ef6bf680fec5319e6474b5179f63
Samir Taborda <stabordasierra@gmail.com>
2024-03-31 21:49:06 +1100

    some global styles added

--- src/App.js
+++
@@ -1,9 +0,0 @@
-
-export const App = () => {
-  return (
-    <>
-      <h1>Gif app</h1>
-    </>
-  );
-}
-
---
+++ src/GifApp.js
@@ -0,0 +1,16 @@
+
+export const GifApp = () => {
+  return (
+    <>
+      {/* titulo */}
+
+      <h1>Gif app</h1>
+      {/* Input */}
+
+      {/* Listado de gif */}
+
+      {/* gif items */}
+    </>
+  );
+}
+
