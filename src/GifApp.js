9b0a07df361d56c89f6975a5f615d92a7f869464
Samir Taborda <stabordasierra@gmail.com>
2024-04-01 08:42:40 +1100

    Gif list render, AddCategory component working

--- src/GifApp.js
+++ src/GifApp.js
@@ -1,13 +1,34 @@
+import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GifApp = () => {
+
+  const [categories, setCategories] = useState([]);
+
+  const onAddCategory = ( newCategory ) => {
+
+    if ( categories.includes(newCategory) ) return;
+    setCategories([ newCategory, ...categories ]);
+  };
+
   return (
     <>
-      {/* titulo */}
+      {/* title */}
 
       <h1>Gif app</h1>
       {/* Input */}
-
-      {/* Listado de gif */}
+      <AddCategory
+        onNewCategory = { onAddCategory }
+        // setCategories={ setCategories }
+      />
+      {/* gif list */}
+      <ol>
+        { categories.map( category => {
+              return <li key={ category }>{ category }</li>
+            }
+          )
+        }        
+      </ol>
 
       {/* gif items */}
     </>
