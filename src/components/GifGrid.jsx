c572afa33dcb5cd6e5758e815379420d68fca090
Samir Taborda <stabordasierra@gmail.com>
2024-04-01 15:05:48 +1100

    getGif working in GifGrid component

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs";
 
 
 export const GifGrid = ({ category }) => {
+
+    const [images, setImages] = useState([]);
+
+    const getImages = async() => {
+        const newImages = await getGifs( category );
+        setImages(newImages);
+    }
+
+    useEffect( () => {
+        getImages();
+    }, [])
+
     return (
         <>
             <h3>{ category }</h3>
-            <p>Hola mundo { category }</p>
+            <ol>
+                {
+                    images.map(({ id, title }) => (
+                        <li key={id}>{ title }</li>
+                    ))
+                }
+            </ol>
         </>
     )
 }
